05155b2d4802800a1835d84ce384b22b52e7b828
obidullah01 <muhaiminul679@gmail.com>
2021-08-31 14:32:07 +0600

    updated

---
+++ history.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <p>Page 3 [History]</p> 
+   <p>Conversion site </p>
+   1. <a href="home.php">Home</a> 2. <a href="conversion.php">Conversion Rate</a> 3. <a href="history.php">History</a>
+
+   <p>Conversion History</p>
+   <form>
+<input type="text" size="30" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
+</form>
+<br>
+<br>
+   <textarea name="" id="" cols="30" rows="10"></textarea>
+   <script src="./script.js"></script>
+</body>
+</html>
